3fd6be32d63398402846ba8b1b801419fb36e544
Elisa Fernandez Castillo <a01221781@itesm.mx>
2019-03-07 21:55:31 -0600

    New L6

--- L6/L6.py
+++ L6/L6.py
@@ -1,86 +1,155 @@
 import csv
+import timeit
 
-class DataSorter(object):
+
+class sortingData(object):
     def __init__(self):
-        self.lst=[]
-        self.input_file_path_name =""
-        self.output_file_path_name = ""
-    
-    def set_input_data(self, file_path_name):
-        self.input_file_path_name = file_path_name     
-        self.file_to_list()
+        self.lst = []
+        self.time = 0
+        self.sortingMethod = None
 
-    def set_output_data(self, file_path_name):
-        self.output_file_path_name = file_path_name
-
-    def file_to_list(self):
+    def setInputData(self, path):
         try:
-            self.lst=[]
-            with open(self.input_file_path_name, 'r') as f:
-                reader=csv.reader(f)
+            with open(path + '.csv')as f:
+                reader = csv.reader(f)
                 for row in reader:
                     for item in row:
-                        item=float(item)
-                        self.lst.append(item)
-        except ValueError:
-            raise ValueError("List only supports numbers, one of the values of the file is not a number")
+                        try:
+                            item = float(item)
+                            self.lst.append(item)
+                        except ValueError:
+                            pass
         except FileNotFoundError:
-            raise FileNotFoundError("Could not find file in path "+self.input_file_path_name)
+            raise FileNotFoundError("File was not found")
         return self.lst
 
-    def list_to_file(self, p_list):
-        with open(self.output_file_path_name, 'w') as f:
-            writer=csv.writer(f)
+    def set_output_data(self, nameFile):
+        with open(nameFile + '.csv', 'w') as csvFile:
+            writer = csv.writer(csvFile)
             writer.writerow(self.lst)
+        csvFile.close()
 
-    
-    def mergeSort(self,lista):
-        alist=lista
-        if len(alist)>1:
-            mid = len(alist)//2
+    def mergeSort(self, lista):
+        self.sortingMethod = "Merge Sort"
+        start = timeit.default_timer()
+        alist = lista
+        if len(alist) > 1:
+            mid = len(alist) // 2
             lefthalf = alist[:mid]
             righthalf = alist[mid:]
-    
+
             self.mergeSort(lefthalf)
             self.mergeSort(righthalf)
-    
-            i=0
-            j=0
-            k=0
+
+            i = 0
+            j = 0
+            k = 0
             while i < len(lefthalf) and j < len(righthalf):
                 if lefthalf[i] < righthalf[j]:
-                    alist[k]=lefthalf[i]
-                    i=i+1
+                    alist[k] = lefthalf[i]
+                    i = i + 1
                 else:
-                    alist[k]=righthalf[j]
-                    j=j+1
-                k=k+1
-    
+                    alist[k] = righthalf[j]
+                    j = j + 1
+                k = k + 1
+
             while i < len(lefthalf):
-                alist[k]=lefthalf[i]
-                i=i+1
-                k=k+1
-    
+                alist[k] = lefthalf[i]
+                i = i + 1
+                k = k + 1
+
             while j < len(righthalf):
-                alist[k]=righthalf[j]
-                j=j+1
-                k=k+1
+                alist[k] = righthalf[j]
+                j = j + 1
+                k = k + 1
+        stop = timeit.default_timer()
+        self.time = stop - start
 
-    def execute_merge_sort(self):
-        alist = self.file_to_list()
-        self.mergeSort(alist)
-        self.list_to_file(alist)
-    
+    def heap_sort(self):
+        self.sortingMethod = "Heap Sort"
+        starting = timeit.default_timer()
+        end = len(self.lst)
+        start = end // 2 - 1
+        for i in range(start, -1, -1):
+            self.heapify(end, i)
+        for i in range(end - 1, 0, -1):
+            self.swap(i, 0)
+            self.heapify(i, 0)
+        stop = timeit.default_timer()
+        self.time = stop - starting
 
-#srtDt=sortingData()
-#lista=srtDt.setInputData("Book1")
-#srtDt.mergeSort(lista)
-#srtDt.set_output_data("SortedBook1")
-#print(srtDt.lst)
-'''sorter = DataSorter()
-inputFile = input("Input name of the file to sort")
-sorter.set_input_data(inputFile)
-outputFile = input("Set file name to store sorted output")
-sorter.set_output_data(outputFile)
+    def swap(self, i, j):
+        self.lst[i], self.lst[j] = self.lst[j], self.lst[i]
 
-sorter.execute_merge_sort()'''
+    def heapify(self, end, i):
+        l = 2 * i + 1
+        r = 2 * i + 2
+
+        max = i
+        if l < end and self.lst[i] < self.lst[l]:
+            max = l
+        if r < end and self.lst[max] < self.lst[r]:
+            max = r
+        if max != i:
+            self.swap(i, max)
+            self.heapify(end, max)
+
+    def quickSort(self):
+        self.sortingMethod = "Quick Sort"
+        start = timeit.default_timer()
+        self.quickSortHelper(self.lst, 0, len(self.lst) - 1)
+        stop = timeit.default_timer()
+        self.time = stop - start
+
+    def quickSortHelper(self, alist, first, last):
+        if first < last:
+            splitpoint = self.partition(alist, first, last)
+
+            self.quickSortHelper(alist, first, splitpoint - 1)
+            self.quickSortHelper(alist, splitpoint + 1, last)
+
+    def partition(self, alist, first, last):
+        pivotvalue = alist[first]
+
+        leftmark = first + 1
+        rightmark = last
+
+        done = False
+        while not done:
+
+            while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+                leftmark = leftmark + 1
+            while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+                rightmark = rightmark - 1
+
+            if rightmark < leftmark:
+                done = True
+            else:
+                temp = alist[leftmark]
+                alist[leftmark] = alist[rightmark]
+                alist[rightmark] = temp
+        temp = alist[first]
+        alist[first] = alist[rightmark]
+        alist[rightmark] = temp
+
+        return rightmark
+
+    def getPerformanceData(self):
+        n = len(self.lst)
+        msg = "Number of sorted records: ", n, "Time spent in execution:" + str(
+            self.time), "Sorting Method:" + self.sortingMethod
+        print(msg)
+
+# start = timeit.default_timer()
+# srtDt=sortingData()
+# lista=srtDt.setInputData("Book1")
+# print(srtDt.lst)
+# srtDt.getPerformanceData2()
+# srtDt.mergeSort(lista)
+# srtDt.heap_sort()
+# srtDt.quickSort()
+# srtDt.set_output_data("SortedBook1")
+# print(srtDt.lst)
+# srtDt.getPerformanceData()
+# stop=timeit.default_timer()
+
