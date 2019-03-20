48a5d1b6bd26eb9781f88abb5dae55987677060f
Roberto Rojas <roberto.rojas@oracle.com>
2019-03-19 23:38:26 -0600

    Added tictacBoard unit test

--- L8/test_TicTacToe.py
+++ L8/test_TicTacToe.py
@@ -1,6 +1,7 @@
 import unittest
 import tictac
 import board
+import tictacBoard
 
 
 class TestTictac(unittest.TestCase):
@@ -15,7 +16,51 @@ class TestTictac(unittest.TestCase):
         #msg="|  |  | \n------------\n |  |  | \n------------\n |  | X | \n------------\n"
         #printedBoard=brd.draw()
         #self.assertEqual(printedBoard,msg)
+    
+    def test_tictacBoard(self):
+        brd = tictacBoard.TictacBoard()
+        self.assertFalse(brd.full_row_equals(0, "X"))
+        brd.set_state(0, 0, "X")
+        brd.set_state(1, 0, "X")
+        brd.set_state(2, 0, "X")
+        self.assertTrue(brd.full_row_equals(0, "X"))
+
+        self.assertFalse(brd.full_column_equals(0, "X"))
+        brd.set_state(0, 0, "X")
+        brd.set_state(0, 1, "X")
+        brd.set_state(0, 2, "X")
+        self.assertTrue(brd.full_column_equals(0, "X"))
+
+        self.assertFalse(brd.diagonal_equals("X"))
+        self.assertFalse(brd.antidiagonal_equals("X"))
+        brd.set_state(1, 1, "X")
+        brd.set_state(2, 2, "X")
+        self.assertTrue(brd.diagonal_equals("X"))
+        self.assertTrue(brd.antidiagonal_equals("X"))
+
+        self.assertEqual(brd.get_available_coords(), [[1, 2], [2, 1]] )
+
+        brd_win = tictacBoard.TictacBoard()
+        self.assertFalse(brd_win.check_win("X"))
+        brd_win.set_state(0, 2, "X")
+        brd_win.set_state(1, 1, "X")
+        brd_win.set_state(2, 0, "X")
+        self.assertTrue(brd_win.check_win("X"))
+        brd_win.set_state(0, 0, "X")
+        brd_win.set_state(1, 1, "X")
+        brd_win.set_state(2, 2, "X")
+        self.assertTrue(brd_win.check_win("X"))
+        brd_win.set_state(1, 0, "X")
+        self.assertTrue(brd_win.check_win("X"))
+        brd_win.set_state(0, 1, "X")
+        self.assertTrue(brd_win.check_win("X"))
+        self.assertFalse(brd_win.check_tie())
+        brd_win.set_state(2, 1, "0")
+        brd_win.set_state(1, 2, "0")
+        self.assertTrue(brd_win.check_tie())
+        brd.draw_tictac()
         
-        
-        
+
+if __name__ == '__main__':
+    unittest.main()
         
